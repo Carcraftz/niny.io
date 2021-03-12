ff9a41100412a923db76d6a1a7ab491a208973af
Restyled.io <commits@restyled.io>
2021-03-12 00:02:38 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.enable("trust proxy");
 const rateLimit = require("express-rate-limit");
 app.all("*", checkHttps);
 const limiter = rateLimit({
-  windowMs: 60 * 1000,
-  max: 60, // You can make max 60 links per min (pretty generous tbh)
+  windowMs : 60 * 1000,
+  max : 60, // You can make max 60 links per min (pretty generous tbh)
 });
 app.use(limiter);
 // add handlebars for templating
@@ -26,7 +26,8 @@ const Keyv = require("keyv");
 const links = new Keyv("sqlite://database.sqlite");
 // regex used to validate links
 // regex-weburl.js by Diego Perini0;
-var urlregex = new RegExp(/^(?:(?:(?:https?|ftp?|http):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i);
+var urlregex = new RegExp(
+    /^(?:(?:(?:https?|ftp?|http):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i);
 
 // generate random links if no vanity url is provided
 const words = require("friendly-words");
@@ -39,32 +40,33 @@ app.use(express.static("public"));
 // https redirect
 
 // listen on port 3000
-app.listen(3000, () => {
-  console.log(`psty app listening at 3000`);
-});
+app.listen(3000, () => { console.log(`psty app listening at 3000`); });
 
 // serve webpage
 app.get("/", (request, response) => {
   response.render("index", {
-    hostname: hostname.split("://")[1],
-    layout: false,
+    hostname : hostname.split("://")[1],
+    layout : false,
   });
 });
 
 // validate that webpage is being served at url
 // uses axios and RegExp
-async function getValidUrl(url){
-  if (url.trim()=="") return false;
-  if (!url.match(urlRegEx)) return false;
+async function getValidUrl(url) {
+  if (url.trim() == "")
+    return false;
+  if (!url.match(urlRegEx))
+    return false;
   try {
-      if (url.substring(0,6)!="https:" && url.substring(0,5)!="http:") {
-          let [test1, test2, validUrl] = ["https://" + url, "http://" + url, false];
-          let [res1, res2]  = [await axios.get(test1), await axios.get(test2)]
-          validUrl = (res1 != null ? test1 : (res2 != null ? test2 : false));
-      } else {
-          let res1 = await axios.get(url)
-          validUrl = (res1 != null ? url : false)
-      }
+    if (url.substring(0, 6) != "https:" && url.substring(0, 5) != "http:") {
+      let [test1, test2, validUrl] =
+          [ "https://" + url, "http://" + url, false ];
+      let [res1, res2] = [ await axios.get(test1), await axios.get(test2) ]
+      validUrl = (res1 != null ? test1 : (res2 != null ? test2 : false));
+    } else {
+      let res1 = await axios.get(url)
+      validUrl = (res1 != null ? url : false)
+    }
   } catch (e) {
     // some webpages terminate fetch requests early
     validUrl = e.request.finished ? e.request._redirectable._currentUrl : false
@@ -87,13 +89,10 @@ app.post("/shortenlink", async (request, response) => {
       if (url != null && url != false) {
         links.set(json.vanity, url);
         response.json({
-          status:
-            "Success! You can view your link at " +
-            hostname +
-            "/" +
-            json.vanity,
-          vanity: json.vanity,
-          url: hostname + "/" + json.vanity,
+          status : "Success! You can view your link at " + hostname + "/" +
+                       json.vanity,
+          vanity : json.vanity,
+          url : hostname + "/" + json.vanity,
         });
       } else {
         response.status(400).send("URL invalid");
@@ -151,7 +150,7 @@ async function generateuuid() {
   }
 }
 async function getwords(count, seperator) {
-  const { predicates, objects } = words;
+  const {predicates, objects} = words;
   const pCount = predicates.length;
   const oCount = objects.length;
   const output = [];
